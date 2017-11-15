852ca2d56292feb3ce8cbde7b040509380d014e3
Zack Siri <zack@codemy.net>
2017-11-15 14:19:44 +0700

    separate page into separate component

--- app/layouts/application.js
+++ app/layouts/application.js
@@ -1,18 +1,8 @@
 import React, { createElement } from 'react'
-import { routeNode } from 'react-router5';
 
-import * as pages from '../pages'
+import Page from './page'
 
 import './application.sass'
-
-const Page = routeNode('')((props) => {
-  const { route } = props;
-
-  const resource = route.name.split('.')[0]
-  const page = route.name.split('.')[-1] || 'collection'
-
-  return createElement(pages[resource][page])
-})
 
 class Application extends React.Component {
   render() {
