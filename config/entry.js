8c87fa29d01dd1922c45be11be9ab1e8ba65d53b
Zack Siri <zack@codemy.net>
2017-11-16 11:01:17 +0700

    changed entry location

--- config/entry.js
+++ config/entry.js
@@ -1,6 +1,6 @@
 const { resolve } = require('path');
 
 module.exports = {
-  app: resolve(__dirname, '..', 'app'),
-  vendor: resolve(__dirname, '..', 'vendor')
+  app: resolve(__dirname, '..', 'app', 'index'),
+  vendor: resolve(__dirname, '..', 'app', 'vendor')
 };
