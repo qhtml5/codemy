cb056daca0fdbe14da478d88b2c8c89134e6c811
Zack Siri <zack@codemy.net>
2017-11-17 18:14:08 +0700

    import post collection component

--- app/pages/posts/collection.js
+++ app/pages/posts/collection.js
@@ -1,10 +1,66 @@
-import React from 'react'
+import React      from 'react'
+import Scrollbars from 'react-custom-scrollbars'
+import { extendObservable } from 'mobx'
+import { inject, observer }  from 'mobx-react'
+import _             from 'lodash'
 
-class Collection extends React.PureComponent {
+import Spinner       from 'front/components/Spinner'
+
+import Post          from './Post'
+import './collection.sass'
+
+@inject('endpoints') @observer
+class Collection extends React.Component {
+  constructor(props) {
+    super(props)
+
+    const { endpoints } = this.props
+    const { studio } = endpoints
+
+    // Finish this
+    extendObservable(this, {
+      posts: 
+    })
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { posts } = this.props
+    posts.findAll({ search: true, channel_id: nextProps.channelId, page: 1 })
+  }
+
+  componentDidMount() {
+    const { posts, channelId } = this.props
+    posts.findAll({ search: true, channel_id: channelId, page: 1 })
+  }
+
+  paginate = _.debounce(() => { 
+    const { posts } = this.props
+    const { currentPage } = posts
+
+    posts.findAll({ 
+      search: true, channel_id: this.props.channelId, page: currentPage + 1 
+    }, { replace: false })
+  }, 800)
+
+  handleScroll = scroll => {
+    if ((scroll.top * 100) >= 90) this.paginate()
+  }
+
   render() {
+    const { posts } = this.props
+    const { isLoading, collection } = posts
+
     return (
-      <div>Posts</div>
-    );
+      <Scrollbars autoHeight autoHeightMin={`calc(100vh - 52px)`} 
+                  onScrollFrame={this.handleScroll} universal ref={node => { this.scrollbar = node }}>
+        <div styleName='collection'>
+          {collection.map((post, index) =>
+            <Post key={post.id} {...post} index={index % 24} pathname={posts.route({ id: post.slug })} />
+          )}
+          <Spinner active={isLoading} />
+        </div>
+      </Scrollbars>
+    )
   }
 }
 
---
+++ app/stores/posts.js
@@ -0,0 +1,53 @@
+import { action, observable } from 'mobx' 
+import { Connect } from 'fronto-connect'
+
+export default class extends Connect { 
+  namespace = 'v1'
+  resource  = 'posts'
+
+  @observable currentPage = 0
+  @observable totalPages = 1
+
+  @action setTotalPages = pages =>
+    this.totalPages = pages
+
+  @action setCurrentPage = (page) =>
+    this.currentPage = page
+
+  replaceCollection = () => {
+    this.clearCollection()
+    this.setCurrentPage(0)
+    this.setTotalPages(1)
+  }
+
+  paginate = (body, replace = true) => {
+    if (replace) {
+      this.setCollection(body.data)
+      this.setCurrentPage(1)
+    } else { 
+      this.appendToCollection(body.data)
+      this.setCurrentPage(this.currentPage + 1) 
+    }
+
+    this.setTotalPages(body.meta.total_pages)
+  }
+
+  findAll(parameters, options = { replace: true }) { 
+    if (options.replace) this.replaceCollection()
+
+    if (this.currentPage < this.totalPages) {
+      this.setIsLoading(true)
+      this.call({ parameters, type: 'get', query: true }, {
+        200: (body) => this.paginate(body, options.replace)
+      })
+    }
+  }
+
+  findBy(parameters) {
+    this.setIsLoading(true)
+    this.clearSelected()
+    this.call({ parameters, type: 'get' }, {
+      200: (body) => this.setSelected(body.data)
+    })
+  }
+}
