434b53400ab3d4b4196934bea9d9946cc1f6c859
Zack Siri <zack@codemy.net>
2017-12-13 10:47:05 +0700

    remove resend

--- app/layouts/confirmation.js
+++ app/layouts/confirmation.js
@@ -6,16 +6,8 @@ export default (props = {user: null, layout: null}) => {
     notification: {
       message: t('confirm_account'),
       key: 'confirm',
-      action: t('confirm_action'),
       dismissAfter: false,
-      title: t('confirm_title'),
-      onClick: (notification, dismiss) => { 
-        confirmation.resend()
-      }
-    },
-    resend() {
-      console.log(props.user.current.email)
-      console.log('resent!')
+      title: t('confirm_title')
     },
     notify() {
       props.layout.appendNotification(this.notification)
