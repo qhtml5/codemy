c1d45242ab80064b291f6bb61abc703a80c6d2b4
Zack Siri <zack@codemy.net>
2018-03-30 22:32:21 +0700

    remove debounce search key up

--- src/pages/posts/collection/keyword.js
+++ src/pages/posts/collection/keyword.js
@@ -1,31 +1,30 @@
 import React from 'react'
 import { observer, inject } from 'mobx-react'
 
-import _ from 'lodash'
-
 import './keyword.sass'
 
 @observer
 class Keyword extends React.Component {
-  searchPosts = _.debounce(() => {
+  searchPosts = (e) => {
+    e.preventDefault()
+
     const { router, route } = this.props
     const { params } = route
     const keyword = this.keyword.value
 
     if (params.channelId && keyword.length > 0)
-      router.navigate('channels.show.search', { 
-        channelId: params.channelId, keyword 
+      router.navigate('channels.show.search', {
+        channelId: params.channelId, keyword
       })
     else if (params.channelId)
-      router.navigate('channels.show', { 
-        channelId: params.channelId 
+      router.navigate('channels.show', {
+        channelId: params.channelId
       })
     else if (keyword.length > 0)
       router.navigate('posts.search', { keyword })
     else
       router.navigate('posts')
-
-  }, 600)
+  }
 
   componentDidUpdate() {
     const { route } = this.props
@@ -41,12 +40,11 @@ class Keyword extends React.Component {
         <div className='pure-u-3-24 pure-u-md-1-24 pure-u-sm-2-24' styleName='icon'>
           <i className='material-icons'>search</i>
         </div>
-        <form className='pure-form pure-u-20-24 pure-u-md-11-24 pure-u-sm-3-4'>
+        <form className='pure-form pure-u-20-24 pure-u-md-11-24 pure-u-sm-3-4' onSubmit={this.searchPosts}>
           <fieldset>
             <input ref={node => this.keyword = node} type='text'
                    placeholder='Keyword Search'
-                   defaultValue={keyword}
-                   onKeyUp={this.searchPosts} />
+                   defaultValue={keyword} />
           </fieldset>
         </form>
       </div>
